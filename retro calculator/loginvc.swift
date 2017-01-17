d11b3799e5528da4af35af1c6369b4aec8ee9b09
carlosBabel <carlos.babel@gmail.com>
2017-01-17 22:56:31 +0100

    Sounds working

---
+++ retro calculator/loginvc.swift
@@ -0,0 +1,19 @@
+//
+//  loginvc.swift
+//  retro calculator
+//
+//  Created by Carlos on 17/1/17.
+//  Copyright © 2017 Chichiri Games. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
